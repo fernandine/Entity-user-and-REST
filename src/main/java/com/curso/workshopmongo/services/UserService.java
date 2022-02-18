e9712af5fffb771f91a61c0ffa419453034b5304
Jean Fernandine <jfernandine@gmail.com>
2022-02-18 17:05:56 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/curso/workshopmongo/services/UserService.java
+++ src/main/java/com/curso/workshopmongo/services/UserService.java
@@ -38,6 +38,20 @@ public class UserService {
 		repo.delete(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = repo.findOne(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+	
+	
+	
 	public User fromDTO(UserDTO objDTO ) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
