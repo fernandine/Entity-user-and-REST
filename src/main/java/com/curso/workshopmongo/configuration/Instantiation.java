d1913543ace9087f214a5c9304e344df7f06e63c
Jean Fernandine <jfernandine@gmail.com>
2022-02-19 10:31:48 -0300

    Referenciando os Posts do usuario

--- src/main/java/com/curso/workshopmongo/configuration/Instantiation.java
+++ src/main/java/com/curso/workshopmongo/configuration/Instantiation.java
@@ -42,6 +42,9 @@ public class Instantiation implements CommandLineRunner {
 		Post post2 = new Post(null, sdf.parse(("01/02/2022"), "Bom dia", "Vou viajar hoje", new AuthorDTO(maria)));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2 ));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 	
 }
